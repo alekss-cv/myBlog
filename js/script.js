309f506517e6acd406ed5c53f6d0e4dc6c962472
alekss-cv <91898963+alekss-cv@users.noreply.github.com>
2022-12-01 19:25:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,6 +19,5 @@ setInterval(function(){
 } , 30000);
 setInterval(function(){
     bcgImg.style.backgroundImage = "linear-gradient(rgba(0 , 0 , 0 ,0.4) , rgba(0 , 0 , 0 ,0.4)) ,  url(\"../img/img_bloks_bcg/block_3_bcg.png\")"
-} , 6
-
+} , 660000)
 
